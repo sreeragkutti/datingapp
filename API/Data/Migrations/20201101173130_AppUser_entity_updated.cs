8402afb050c3824df3c25936e4896014fc20d1ad
sreeragkutti <sreerag801@outlook.com>
2020-11-01 23:04:27 +0530

    Entityframework added and first migration code updated

---
+++ API/Data/Migrations/20201101173130_AppUser_entity_updated.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Data.Migrations
+{
+    public partial class AppUser_entity_updated : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Gender",
+                table: "Users",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Gender",
+                table: "Users");
+        }
+    }
+}
