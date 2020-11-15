a5dfaafa296d23b6afcc8d0f1cf6ed3b559106d8
sreeragkutti <sreerag801@outlook.com>
2020-11-15 19:49:51 +0530

    client app created in angular

---
+++ API/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly DataContext _dataContext;
+        public UserController(DataContext context)
+        {
+            _dataContext = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            return await _dataContext.Users.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUsers(int id)
+        {
+            return await _dataContext.Users.FindAsync(id);
+        }
+    }
+}
--- API/Startup.cs
+++ API/Startup.cs
@@ -30,6 +30,7 @@ namespace API
                 options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
             services.AddControllers();
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,6 +44,13 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x => 
+            {
+                x.AllowAnyHeader()
+                .AllowAnyMethod()
+                .WithOrigins("http://localhost:4200");
+            });
 
             app.UseAuthorization();
 
